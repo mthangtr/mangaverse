87de2d178eb34e8eababd42ef17053b8cb69724e
cccccc9712 <cccccc9712@gmail.com>
2024-04-16 22:50:19 +0700

    search results

--- src/layouts/components/Search/Search.js
+++ src/layouts/components/Search/Search.js
@@ -1,17 +1,99 @@
 import styles from './Search.module.scss';
 import classNames from 'classnames/bind';
 import Button from '@/components/Button';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Tippy from '@tippyjs/react';
+import { useEffect, useRef, useState } from 'react';
+import { useDebounce } from '@/hooks/useDebounce';
+import { Wrapper as PopperWrapper } from '@/components/Popper';
+import SearchItem from '@/components/SearchItem';
 
 const cx = classNames.bind(styles);
 
 function Search() {
+    const [searchValue, setSearchValue] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+    const [showResult, setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const debouncedValue = useDebounce(searchValue, 500);
+
+    const inputRef = useRef();
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setSearchResult([1, 2, 3]);
+    //     }, 1000);
+    // }, []);
+
+    const handleClear = () => {
+        setSearchValue('');
+        setSearchResult([]);
+        inputRef.current.focus();
+    };
+
+    const handleHideResult = () => {
+        setShowResult(false);
+    };
+
+    const handleChange = (e) => {
+        const searchValue = e.target.value;
+        if (!searchValue.startsWith(' ')) {
+            setSearchValue(searchValue);
+        }
+    };
+
     return (
-        <div className={cx('wrapper')}>
-            <input type="text" placeholder="Search" />
-            <Button className={cx('search-btn')}><FontAwesomeIcon icon={faSearch} /></Button>
-        </div>
+        <div>
+            <Tippy
+                interactive={true}
+                visible={showResult && searchResult.length > 0}
+                onClickOutside={handleHideResult}
+                placement='bottom-start'
+                render={(attrs) => (
+                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                        <PopperWrapper className={'search-popper'}>
+                            <h4 className={cx('search-title')}>Search Results</h4>
+                            <SearchItem />
+                            <SearchItem />
+                            <SearchItem />
+                            <SearchItem />
+                            <SearchItem />
+                            <SearchItem />
+                            <SearchItem />
+                            <SearchItem />
+                            <SearchItem />
+                            <SearchItem />
+                            <SearchItem />
+                            <SearchItem />
+                            <SearchItem />
+                            <SearchItem />
+                            <SearchItem />
+                            <SearchItem />
+                        </PopperWrapper>
+                    </div>
+                )}
+            >
+                <div className={cx('search')}>
+                    <input
+                        type="text" placeholder="Search"
+                        ref={inputRef}
+                        value={searchValue}
+                        spellCheck={false}
+                        onChange={handleChange}
+                        onFocus={() => setShowResult(true)}
+                    />
+                    {!!searchValue && !loading && (
+                        <button className={cx('clear')} onClick={handleClear}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                    )}
+                    {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
+                    <Button className={cx('search-btn')}><FontAwesomeIcon icon={faSearch} /></Button>
+                </div>
+            </Tippy>
+        </div >
     );
 }
 
